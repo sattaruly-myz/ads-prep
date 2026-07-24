8caeb8bd2ff7ab0b63308e56a67ffa86e7364a84
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-07-24 17:30:16 +0500

    feat: add stack implementation

---
+++ week01/arays.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void d1(){
+    
+}
+
+
+int main(){
+    string choose; cout<<"Choose type of arrays for calculate avrg and find max, min value"<<endl; cout<<"If 1d array write 1d, else if 2d array write 2d:D"<<endl; cin>>choose;
+    if (choose=="1d"){
+        d1();
+    }
+
+    return 0;
+}
